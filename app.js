702e9febcf220bee266e11aa1be006186a911ac0
olds1423 <olds1423@gmail.com>
2016-06-07 19:21:51 -0700

    Pseudo outlined how I am going to lay out the function

---
+++ app.js
@@ -0,0 +1,14 @@
+// Javascript portion of my Campefire coffee project
+var pikePlaceMarket = {
+  minCustomersHr: 14,
+  maxCustomersHr: 35,
+  cupsPerCust: 1.2,
+  toGoPoundsPerCustomer: 0.34,
+  //Random number method will go in here
+  // Projected number of cups sold per hour (at this location)
+  // add all projected hours together to get cups per day
+  //Projected number of pounds sold per hour
+  // Total number of beans needed to make cups and fulfill to-go bean orders by hours
+  //The number of employees needed you will need to figure out the total amount at each location then break it down
+};
+pikePlaceMarket();
